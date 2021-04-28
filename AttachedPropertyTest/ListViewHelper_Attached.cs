0b0cec3206f198d75465df5a443854bd866b093f
hsh <prisonsh@naver.com>
2021-04-28 17:13:20 +0900

    Attached,Behavior로 SelectedItems 바인딩

--- AttachedPropertyTest/ListViewHelper_Attached.cs
+++ AttachedPropertyTest/ListViewHelper_Attached.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -6,12 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace AttachedPropertyTest
 {
     public class ListViewHelper_Attached
     {
-
         public static readonly DependencyProperty SelectedItemsProperty
             = DependencyProperty.RegisterAttached("SelectedItems",
                                                                   typeof(INotifyCollectionChanged),
@@ -29,7 +30,130 @@ namespace AttachedPropertyTest
 
         public static void SelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var source = d as ListBox;
+            if (!(d is ListBox)) return;
+
+            ListBox associatedObject = d as ListBox;
+            NotifyCollectionChangedEventHandler handler = (sender, args) =>
+            {
+                if (associatedObject != null)
+                {
+                    if (_isUpdatingFromSource)
+                        return;
+
+                    try
+                    {
+                        _isUpdatingFromTarget = true;
+                        IList selectedItems = associatedObject.SelectedItems;
+                        if (args.Action == NotifyCollectionChangedAction.Reset)
+                        {
+                            selectedItems.Clear();
+                            return;
+                        }
+                        if (args.OldItems != null)
+                        {
+                            foreach (var item in args.OldItems)
+                            {
+                                if (selectedItems.Contains(item))
+                                {
+                                    selectedItems.Remove(item);
+                                }
+                            }
+                        }
+
+                        if (args.NewItems != null)
+                        {
+                            foreach (var item in args.NewItems)
+                            {
+                                if (!selectedItems.Contains(item))
+                                {
+                                    selectedItems.Add(item);
+                                }
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        _isUpdatingFromTarget = false;
+                    }
+                }
+            };
+
+            if (associatedObject !=null && e.OldValue == null)
+            {
+                //초기 Attached상태를 위한듯...
+                associatedObject.SelectionChanged += CollectionChangedFromSource; 
+            }
+
+            if(e.OldValue is INotifyCollectionChanged)
+                (e.OldValue as INotifyCollectionChanged).CollectionChanged -= handler;
+            if (e.NewValue is INotifyCollectionChanged)
+                (e.NewValue as INotifyCollectionChanged).CollectionChanged += handler; ;
+        }
+
+        private static bool _isUpdatingFromSource;  // View(Source)에서 업데이트
+        private static bool _isUpdatingFromTarget;  // VIewModel(Target)에서 업데이트
+
+        private static void CollectionChangedFromTarget(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_isUpdatingFromSource)
+                return;
+
+            _isUpdatingFromTarget = true;
+            try
+            {
+                if (sender is ListBox)
+                {
+                    var associatedObject = sender as ListBox;
+                    var selectedItems = associatedObject.SelectedItems;
+
+                    if (e.Action == NotifyCollectionChangedAction.Reset)
+                    {
+                        selectedItems.Clear();
+                        return;
+                    }
+                    foreach (var item in e.OldItems)
+                    {
+                        selectedItems.Remove(item);
+                    }
+                    foreach (var item in e.NewItems)
+                    {
+                        selectedItems.Remove(item);
+                    }
+                }
+            }
+            finally
+            {
+                _isUpdatingFromTarget = false;
+            }
+        }
+        private static void CollectionChangedFromSource(object sender, SelectionChangedEventArgs e)
+        {
+            if (_isUpdatingFromTarget)
+                return;
+
+            _isUpdatingFromSource = true;
+            try
+            {
+                DependencyObject d = sender as DependencyObject;
+                if (sender is ListBox)
+                {
+                    var associatedObject = sender as ListBox;
+                    var selectedItems = associatedObject.SelectedItems;
+
+                    foreach (var item in e.RemovedItems)
+                    {
+                        selectedItems.Remove(item);
+                    }
+                    foreach (var item in e.AddedItems)
+                    {
+                        selectedItems.Add(item);
+                    }
+                }
+            }
+            finally
+            {
+                _isUpdatingFromSource = false;
+            }
         }
     }
 }
--- AttachedPropertyTest/ListviewHelper_Behavior.cs
+++ AttachedPropertyTest/ListviewHelper_Behavior.cs
@@ -1,23 +1,137 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Interactivity;
 
 namespace AttachedPropertyTest
 {
-    public class ListviewHelper_Behavior : Behavior<FrameworkElement>
+    public class ListviewHelper_Behavior : Behavior<ListBox>
     {
         public static readonly DependencyProperty SelectedItemsProperty
-            = DependencyProperty.RegisterAttached("SelectedItems",
-                                                                  typeof(INotifyCollectionChanged),
+            = DependencyProperty.Register("SelectedItems",
+                                                                  typeof(IList),
                                                                   typeof(ListviewHelper_Behavior),
-                                                                  new PropertyMetadata(null, SelectedItemsChanged));
+                                                                  new UIPropertyMetadata(null, SelectedItemsChanged));
 
-         
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (SelectedItems != null)
+            {
+                AssociatedObject.SelectedItems.Clear();
+                AssociatedObject.SelectionChanged += CollectionChangedFromSource;
+            }
+            foreach (var item in SelectedItems)
+            {
+                AssociatedObject.SelectedItems.Add(item);
+            }
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            if (this.AssociatedObject != null)
+                AssociatedObject.SelectionChanged -= CollectionChangedFromSource;
+        }
+
+        public IList SelectedItems
+        {
+            get => (IList)GetValue(SelectedItemsProperty);
+            set => SetValue(SelectedItemsProperty, value);
+        }
+        public static void SelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = d as ListviewHelper_Behavior;
+
+            if (e.OldValue is INotifyCollectionChanged)
+                (e.OldValue as INotifyCollectionChanged).CollectionChanged -= behavior.CollectionChangedFromTarget;
+            if (e.NewValue is INotifyCollectionChanged)
+                (e.NewValue as INotifyCollectionChanged).CollectionChanged += behavior.CollectionChangedFromTarget;
+        }
+        private static bool _isUpdatingFromSource;  // View(Source)에서 업데이트
+        private static bool _isUpdatingFromTarget;  // VIewModel(Target)에서 업데이트
+
+        private void CollectionChangedFromTarget(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_isUpdatingFromSource)
+                return;
+
+            _isUpdatingFromTarget = true;
+            try
+            {
+                if (e.Action == NotifyCollectionChangedAction.Reset)
+                {
+                    AssociatedObject.SelectedItems.Clear();
+                    return;
+                }
+                foreach (var item in e.OldItems)
+                {
+                    AssociatedObject.SelectedItems.Remove(item);
+                }
+                foreach (var item in e.NewItems)
+                {
+                    AssociatedObject.SelectedItems.Remove(item);
+                }
+            }
+            finally
+            {
+                _isUpdatingFromTarget = false;
+            }
+        }
+        private void CollectionChangedFromSource(object sender, SelectionChangedEventArgs e)
+        {
+            if (_isUpdatingFromTarget)
+                return;
+
+            _isUpdatingFromSource = true;
+            try
+            {
+                foreach (var item in e.RemovedItems)
+                {
+                    AssociatedObject.SelectedItems.Remove(item);
+                }
+                foreach (var item in e.AddedItems)
+                {
+                    AssociatedObject.SelectedItems.Add(item);
+                }
+            }
+            finally
+            {
+                _isUpdatingFromSource = false;
+            }
+        }
+    }
+
+    public class ListviewHelper_Behavior2 : Behavior<ListBox>
+    {
+        public static readonly DependencyProperty SelectedItemsProperty
+            = DependencyProperty.Register("SelectedItems",
+                                                                  typeof(INotifyCollectionChanged),
+                                                                  typeof(ListviewHelper_Behavior2),
+                                                                  new UIPropertyMetadata(null, SelectedItemsChanged));
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (SelectedItems != null)
+            {
+                AssociatedObject.SelectedItems.Clear();
+                AssociatedObject.SelectionChanged += CollectionChangedFromSource;
+            }
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            if (this.AssociatedObject != null)
+                AssociatedObject.SelectionChanged -= CollectionChangedFromSource;
+        }
+
         public INotifyCollectionChanged SelectedItems
         {
             get => (INotifyCollectionChanged)GetValue(SelectedItemsProperty);
@@ -25,7 +139,64 @@ namespace AttachedPropertyTest
         }
         public static void SelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
+            var behavior = d as ListviewHelper_Behavior2;
 
+            if (e.OldValue is INotifyCollectionChanged)
+                (e.OldValue as INotifyCollectionChanged).CollectionChanged -= behavior.CollectionChangedFromTarget;
+            if (e.NewValue is INotifyCollectionChanged)
+                (e.NewValue as INotifyCollectionChanged).CollectionChanged += behavior.CollectionChangedFromTarget;
+        }
+        private static bool _isUpdatingFromSource;  // View(Source)에서 업데이트
+        private static bool _isUpdatingFromTarget;  // VIewModel(Target)에서 업데이트
+
+        private void CollectionChangedFromTarget(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_isUpdatingFromSource)
+                return;
+
+            _isUpdatingFromTarget = true;
+            try
+            {
+                if (e.Action == NotifyCollectionChangedAction.Reset)
+                {
+                    AssociatedObject.SelectedItems.Clear();
+                    return;
+                }
+                foreach (var item in e.OldItems)
+                {
+                    AssociatedObject.SelectedItems.Remove(item);
+                }
+                foreach (var item in e.NewItems)
+                {
+                    AssociatedObject.SelectedItems.Remove(item);
+                }
+            }
+            finally
+            {
+                _isUpdatingFromTarget = false;
+            }
+        }
+        private void CollectionChangedFromSource(object sender, SelectionChangedEventArgs e)
+        {
+            if (_isUpdatingFromTarget)
+                return;
+
+            _isUpdatingFromSource = true;
+            try
+            {
+                foreach (var item in e.RemovedItems)
+                {
+                    AssociatedObject.SelectedItems.Remove(item);
+                }
+                foreach (var item in e.AddedItems)
+                {
+                    AssociatedObject.SelectedItems.Add(item);
+                }
+            }
+            finally
+            {
+                _isUpdatingFromSource = false;
+            }
         }
     }
 }
